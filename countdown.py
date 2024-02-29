853f85b7173cd3e28bf339253adfcdc21e83528a
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-02-28 20:56:38 -0500

    program runs and time formatted

--- countdown.py
+++ countdown.py
@@ -8,4 +8,4 @@ for i in range(insert_time, 0, -1):
     time.sleep(1)
     seconds = i % 60 
     minutes = int(i / 60) % 60 # 1 minutes = 60 sec. 
-    print(f"{minutes} : {seconds}")
+    print(f"{minutes:02} : {seconds:02}")
