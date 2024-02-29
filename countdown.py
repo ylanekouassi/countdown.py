796acb8b4936eb795db2ad17bcf22b4842cad4ef
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-02-28 20:46:33 -0500

    code testing

--- countdown.py
+++ countdown.py
@@ -5,4 +5,7 @@ import time
 insert_time = int(input("Enter the time in second: "))
 
 for i in range(insert_time, 0, -1):
-    time.sleep()
+    time.sleep(1)
+    seconds = i % 60 
+    minutes = int(i / 60) % 60 # 1 minutes = 60 sec. 
+    print(f"{minutes} : {seconds}")
