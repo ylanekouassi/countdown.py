ddb80e300ce2855b39b59e1a28630acdd50777df
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-02-28 21:04:48 -0500

    countdown timer completed

--- countdown.py
+++ countdown.py
@@ -8,4 +8,5 @@ for i in range(insert_time, 0, -1):
     time.sleep(1)
     seconds = i % 60 
     minutes = int(i / 60) % 60 # 1 minutes = 60 sec. 
-    print(f"{minutes:02} : {seconds:02}")
+    hours = int(i / 3600) # 1 hour = 3600 sec.
+    print(f"{hours:02} : {minutes:02} : {seconds:02}") #formatted time, 2 digits will be displayed
